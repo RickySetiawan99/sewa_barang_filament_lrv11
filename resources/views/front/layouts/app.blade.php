8454f8ca20199f0f59e487f2e90f840add382728
Ricky Setiawan <rickiricki201533@gmail.com>
2024-09-25 14:36:37 +0700

    favicon home

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -5,6 +5,7 @@
 		<link rel="manifest" href="{{ asset('/manifest.json') }}">
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="icon" type="image/png" href="{{ asset('assets/images/icons/Star.svg') }}">
 		<title>
 			@yield('title')
 		</title>
